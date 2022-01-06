813432c53bfe0401f6780e412567f22a06ac0b48
SeoJunhwan <seojunhwan@kakao.com>
2022-01-06 22:43:26 +0900

    fix: MDX Code Syntax Highlighter fix
    기존 div 에 넣는 방식에서 table 로 변경
    코드가 길어져 overflow 되었을 때 기존의 방식으론
    코드 끝까지 div 로 감싸지 않고 그대로 넘김
    table 로 변경 후 반응형으로 레이아웃 수정

--- components/mdx/Code.tsx
+++ components/mdx/Code.tsx
@@ -2,17 +2,47 @@ import styled from 'styled-components';
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/nightOwl';
 
-const CodeWrapper = styled.code`
-  display: flex;
-  flex-direction: column;
-  /* width: 100%; */
-  max-width: 70rem;
-  margin: 0 auto;
-  padding-bottom: 0.5rem;
-  font-size: 1.5rem;
-  border-radius: 1.5rem;
-  overflow: auto;
+const Container = styled.div`
+  border-radius: 1rem;
+  overflow: hidden;
   box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;
+`;
+
+const CodeInfoBar = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.8rem 1.5rem;
+  user-select: none;
+  background-color: #15151599;
+  span {
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    color: #ff8a64;
+    font-family: Aggro;
+  }
+  div:last-child {
+    display: flex;
+    gap: 0.3rem;
+    span {
+      width: 1rem;
+      height: 1rem;
+      border-radius: 0.5rem;
+      background-color: #ffbd2d;
+      &:first-child {
+        background-color: #fe5f57;
+      }
+      &:last-child {
+        background-color: #29c941;
+      }
+    }
+  }
+`;
+
+const TableWrapper = styled.div`
+  overflow-x: auto;
+  overflow-y: hidden;
+  padding: 1rem 0.5rem;
   &::-webkit-scrollbar {
     height: 1.5rem;
   }
@@ -24,18 +54,52 @@ const CodeWrapper = styled.code`
     background-color: grey;
     border-radius: 1rem;
   }
+  @media screen and (max-width: 768px) {
+    &::-webkit-scrollbar {
+      height: 1rem;
+    }
+  }
 `;
 
-const LineNo = styled.span`
-  margin-right: 1rem;
+const Table = styled.table`
+  display: table;
+  width: 100%;
+  font-size: 1.8rem;
+  line-height: 3rem;
+  vertical-align: middle;
+  @media screen and (max-width: 1000px) {
+    font-size: 1.6rem;
+    line-height: 2rem;
+  }
+  @media screen and (max-width: 768px) {
+    font-size: 0.5rem;
+    line-height: 1.2rem;
+  }
+`;
+
+const CodeWrapper = styled.tbody`
+  display: table-row-group;
+  vertical-align: middle;
+`;
+
+const LineNo = styled.td`
+  text-align: center;
+  padding-right: 0.5rem;
   color: #4a545c;
   transition: all 0.05s ease;
   user-select: none;
+  border-top-left-radius: 0.8rem;
+  border-bottom-left-radius: 0.8rem;
+  & + td {
+    border-top-right-radius: 0.8rem;
+    border-bottom-right-radius: 0.8rem;
+    padding-right: 1rem;
+  }
 `;
 
-const Line = styled.div`
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
+const Line = styled.tr`
+  display: table-row;
+  vertical-align: inherit;
   transition: all 0.05s ease;
   &:hover {
     background-color: #20202099;
@@ -45,64 +109,38 @@ const Line = styled.div`
   }
 `;
 
-const CodeInfoBar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0.8rem 1.5rem;
-  margin-bottom: 0.5rem;
-  user-select: none;
-  background-color: #15151599;
-  span {
-    font-size: 1.5rem;
-    text-transform: uppercase;
-    color: #ff8a64;
-    font-family: Aggro;
-  }
-  div {
-    &:last-child {
-      display: flex;
-      gap: 0.3rem;
-      span {
-        width: 1rem;
-        height: 1rem;
-        border-radius: 0.5rem;
-        background-color: #ffbd2d;
-        &:first-child {
-          background-color: #fe5f57;
-        }
-        &:last-child {
-          background-color: #29c941;
-        }
-      }
-    }
-  }
-`;
-
 export default function Code({ children, className }: { children: string; className: string }) {
   const language = className.replace(/language-/, '');
   return (
-    <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <CodeWrapper className={className}>
-          <CodeInfoBar>
-            <span>{language}</span>
-            <div>
-              <span> </span>
-              <span> </span>
-              <span> </span>
-            </div>
-          </CodeInfoBar>
-          {tokens.map((line, i) => (
-            <Line key={i} {...getLineProps({ line, key: i })}>
-              <LineNo>{i + 1}</LineNo>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </Line>
-          ))}
-        </CodeWrapper>
-      )}
-    </Highlight>
+    <Container>
+      <CodeInfoBar>
+        <span>{language}</span>
+        <div>
+          <span> </span>
+          <span> </span>
+          <span> </span>
+        </div>
+      </CodeInfoBar>
+      <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language as any}>
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <TableWrapper>
+            <Table>
+              <CodeWrapper className={className}>
+                {tokens.map((line, i) => (
+                  <Line key={i} {...getLineProps({ line, key: i })}>
+                    <LineNo>{i + 1}</LineNo>
+                    <td>
+                      {line.map((token, key) => (
+                        <span key={key} {...getTokenProps({ token, key })} />
+                      ))}
+                    </td>
+                  </Line>
+                ))}
+              </CodeWrapper>
+            </Table>
+          </TableWrapper>
+        )}
+      </Highlight>
+    </Container>
   );
 }
