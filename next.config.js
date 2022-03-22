613ed848aa62fbfeabc987a888355a4cc707454d
SeoJunhwan <seojunhwan@kakao.com>
2022-03-22 23:30:19 +0900

    ➕ bundle-analyzer 추가
    번들링 후 불필요한 js 가 있는지, 용량은 어떤지 확인하는 bundle-analyzer 설치

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   locales: ['ko-KR'],
   defaultLocale: 'ko-KR',
   images: {
     domains: ['gamguma-blog.s3.ap-northeast-2.amazonaws.com'],
   },
-};
+});
