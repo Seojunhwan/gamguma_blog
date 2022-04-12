ed31614b3d31b62265692e76992b16ef705c79fd
SeoJunhwan <seojunhwan@kakao.com>
2022-04-12 17:08:37 +0900

    ✨ 구글 애널리틱스 추가, 이스터에그 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import Code from '../components/post/Code';
 import { MDXProvider } from '@mdx-js/react';
 import { RecoilRoot } from 'recoil';
 import ResponsiveImage from '../components/post/ResponsiveImage';
+import Script from 'next/script';
+import { useEffect } from 'react';
 
 const components = {
   code: Code,
@@ -13,12 +15,25 @@ const components = {
 };
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    console.log('%c감구마 블로그', `color:#BAABDA; font-size: 4rem; font-weight: bold`);
+  }, []);
   return (
     <>
       <Head>
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1 user-scalable=no' />
       </Head>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-LYR2W8NBVJ' strategy='afterInteractive' />
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-LYR2W8NBVJ');
+        `}
+      </Script>
       <RecoilRoot>
         <MDXProvider components={components}>
           <Layout>
