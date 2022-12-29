af2b4d0bd48d87e02143d2bfea200e5b89f0a308
SeoJunhwan <seojunhwan@kakao.com>
2022-12-30 03:21:01 +0900

    feat: getRelativeDate
    getRelativeDate
      - Intl 활용해 과거의 특정 Date 로부터 현재까지의 상대적인 시간을 반환하는 함수 구현
    
    getRelativeDate
      - Date 간 차이를 origin, day, hour, minute 포멧으로 반환
    
    Signed-off-by: SeoJunhwan <seojunhwan@kakao.com>

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,14 +1,3 @@
-export const dateFormatter = (dateSrc: string) => {
-  const date = new Date(dateSrc.replace(/-/g, '/'));
-  return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
-};
-
-export const dateToTimeFormatter = (dateSrc: Date) => {
-  return `${dateSrc.getFullYear()}-${
-    dateSrc.getMonth() + 1
-  }-${dateSrc.getDate()} ${dateSrc.getHours()}:${dateSrc.getMinutes()}`;
-};
-
 export const throttle = (callback: Function, delay: number) => {
   let timer: ReturnType<typeof setTimeout> | null = null;
 
