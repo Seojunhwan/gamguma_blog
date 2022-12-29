1673e4128c8d57c309571d65c7ed387a2d2af554
SeoJunhwan <seojunhwan@kakao.com>
2022-12-30 04:22:43 +0900

    feat: pretendard 폰트 추가 및 next font, tailwind 설정
    Signed-off-by: SeoJunhwan <seojunhwan@kakao.com>

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import localFont from '@next/font/local';
+import { ReactNode, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { useSetRecoilState } from 'recoil';
@@ -9,8 +10,59 @@ import Header from './Header';
 import Footer from './Footer';
 
 interface Props {
-  children: React.ReactNode;
+  children: ReactNode;
 }
+
+const pretendard = localFont({
+  src: [
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-Thin.subset.woff2',
+      weight: '100',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-ExtraLight.subset.woff2',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-Light.subset.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-SemiBold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-ExtraBold.subset.woff2',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../styles/fonts/Pretendard/woff2-subset/Pretendard-Black.subset.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
+});
 
 export default function Layout({ children }: Props) {
   const setIsDark = useSetRecoilState(isDarkAtom);
@@ -29,7 +81,7 @@ export default function Layout({ children }: Props) {
       {/* TODO: ThemeProvider 제거 */}
       <ThemeProvider theme={lightTheme}>
         <Header />
-        <main className='mx-auto max-w-5xl pt-24'>{children}</main>
+        <main className={`${pretendard.variable} mx-auto max-w-5xl pt-24 font-sans`}>{children}</main>
         <Footer />
       </ThemeProvider>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['var(--font-pretendard)', ...fontFamily.sans],
+    },
   },
   plugins: [require('@tailwindcss/forms')],
   darkMode: 'class',
