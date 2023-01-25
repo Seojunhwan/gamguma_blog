4a7deb2ac4226fd71327f7aeb598ee693ccf6046
SeoJunhwan <seojunhwan@kakao.com>
2023-01-26 03:55:42 +0900

    feat: simpleIntroduction

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -11,12 +11,21 @@ export default function Header() {
     <header
       className={cls(
         'fixed top-0 z-10 w-full bg-white bg-opacity-95 py-6 backdrop-blur-[1px] transition-all duration-300 ease-in-out',
-        isScrollDown ? 'h-16' : 'h-32 bg-transparent',
+        isScrollDown ? 'h-16 border-b-[1px]' : 'h-32 bg-transparent',
       )}
     >
       <div className='mx-auto flex h-full w-full max-w-5xl items-center justify-between px-4 lg:px-2'>
+        <div className='space-x-2 text-center'>
+          {/* TODO: <Image src=''/> */}
+          <Link href='/'>
+            <h1 className='relative font-aggro text-3xl font-medium text-gray-800'>
+              감구마
+              <span className='absolute -right-2 bottom-1 inline-block aspect-square h-2 w-2 rounded-full bg-[#867459] shadow-sm' />
+            </h1>
+          </Link>
+        </div>
         <div className='max-w-[20%] basis-1/5'>
-          <span onClick={() => alert('만드는 중 !')}>
+          {/* <span onClick={() => alert('만드는 중 !')}>
             <svg
               className='aspect-square h-8 w-8 cursor-pointer rounded-md stroke-gray-600 p-1 transition-shadow hover:stroke-amber-400 hover:ring-1 hover:ring-amber-400'
               fill='none'
@@ -31,19 +40,13 @@ export default function Header() {
                 d='M4 6h16M4 12h16M4 18h16'
               />
             </svg>
-          </span>
-        </div>
-        <div className='grow space-x-2 text-center'>
-          {/* TODO: <Image src=''/> */}
-          <Link href='/'>
-            <h1 className='text-xl font-medium text-gray-800'>Gamguma</h1>
-          </Link>
+          </span> */}
         </div>
         <div className='max-w-[20%] basis-1/5'>
-          <input
+          {/* <input
             className='w-full border-none outline-none focus:border-none focus:outline-none'
             type='text'
-          />
+          /> */}
         </div>
       </div>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Pagination from '@components/post/Pagination';
 import type { GetStaticPropsResult } from '@interface';
 import { BLOG_THUMBNAIL } from '@utils';
 import { getPostPaginationPaths, getPostsByPage } from '@utils/mdxUtils';
+import SimpleIntroduce from '@components/SimpleIntroduce';
 
 interface Props extends GetStaticPropsResult {}
 
@@ -19,9 +20,8 @@ export default function Home({ posts, currentPage, paginationLength }: Props) {
         thumbnail={BLOG_THUMBNAIL}
         keywords={['개발', '개발자', '감구마', '42seoul', '42서울', '프론트엔드']}
       />
-      {/* <Head>
-        <link rel='canonical' href={process.env.NEXT_PUBLIC_SITE_URL} />
-      </Head> */}
+      <Head>{/* <link rel='canonical' href={process.env.NEXT_PUBLIC_SITE_URL} /> */}</Head>
+      <SimpleIntroduce />
       <Posts posts={posts} />
       <Pagination currentPage={currentPage} paginationLength={paginationLength} />
     </>
