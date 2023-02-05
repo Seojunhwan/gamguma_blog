368572dc9172f6a3de4f02f448049795695e5a04
Seojunhwan <seojunhwan@kakao.com>
2023-02-06 00:35:49 +0900

    refactor: styled-component 덜어내기

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,68 +1,11 @@
-import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
-import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
-  // TODO: styled-components 덜어내기
-  static async getInitialProps(ctx: DocumentContext) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
   render() {
     return (
       <Html lang='ko'>
         <Head>
-          <link
-            rel='preload'
-            href='/styles/fonts/SpoqaHanSans/SpoqaHanSansNeo-Regular.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
-          />
-          <link
-            rel='preload'
-            href='/styles/fonts/SpoqaHanSans/SpoqaHanSansNeo-Bold.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
-          />
-          <link
-            rel='preload'
-            href='/styles/fonts/GmarketSans/GmarketSansTTFMedium.woff2'
-            as='font'
-            type='font/woff2'
-            crossOrigin='anonymous'
-          />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap'
-            rel='stylesheet'
-          />
-          <link rel='preconnect' href='https://fonts.googleapis.com' />
-          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Source+Code+Pro&display=swap'
-            rel='stylesheet'
-          />
           <link rel='apple-touch-icon' sizes='57x57' href='/favicon/apple-icon-57x57.png' />
           <link rel='apple-touch-icon' sizes='60x60' href='/favicon/apple-icon-60x60.png' />
           <link rel='apple-touch-icon' sizes='72x72' href='/favicon/apple-icon-72x72.png' />
@@ -80,7 +23,7 @@ class MyDocument extends Document {
           <meta name='msapplication-TileColor' content='#ffffff' />
           <meta name='msapplication-TileImage' content='/ms-icon-144x144.png' />
         </Head>
-        <body className='bg-white transition-colors dark:bg-[#111111]'>
+        <body className='bg-background transition-colors dark:bg-[#111111]'>
           <Main />
           <div id='modal' />
           <NextScript />
