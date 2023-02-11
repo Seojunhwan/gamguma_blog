0752c0cdc0ded65b9d3999eae5a40cead39d4d98
Seojunhwan <seojunhwan@kakao.com>
2023-02-12 01:36:08 +0900

    feat: home animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 import type { GetStaticProps } from 'next';
 
 import { CategoryNavigation, Seo } from '@components/common';
 import { SimpleIntroduce } from '@components/profile';
 import { Pagination, Posts } from '@components/post';
-import { BLOG_THUMBNAIL } from '@constants';
+import { animateVariants, BLOG_THUMBNAIL } from '@constants';
 import { getPostPaginationPaths, getPostsByPage } from '@utils/mdxUtils';
 
 import type { GetStaticPropsResult } from '@interface';
@@ -21,14 +22,16 @@ export default function Home({ posts, currentPage, paginationLength }: Props) {
         keywords={['개발', '개발자', '감구마', '42seoul', '42서울', '프론트엔드']}
       />
       <Head>{/* <link rel='canonical' href={process.env.NEXT_PUBLIC_SITE_URL} /> */}</Head>
-      <SimpleIntroduce />
-      <div className='flex items-start space-x-4'>
-        <Posts posts={posts} />
-        <div className='hidden lg:block'>
-          <CategoryNavigation />
+      <motion.div variants={animateVariants} initial='initial' animate='animate' exit='exit'>
+        <SimpleIntroduce />
+        <div className='flex items-start space-x-4'>
+          <Posts posts={posts} />
+          <div className='hidden lg:block'>
+            <CategoryNavigation />
+          </div>
         </div>
-      </div>
-      <Pagination currentPage={currentPage} paginationLength={paginationLength} />
+        <Pagination currentPage={currentPage} paginationLength={paginationLength} />
+      </motion.div>
     </>
   );
 }
