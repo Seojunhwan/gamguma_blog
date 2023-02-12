20b3145510010a060cd3582065768a3572a40258
Seojunhwan <seojunhwan@kakao.com>
2023-02-12 22:59:57 +0900

    design: ImageComponent blur

--- components/post/ResponsiveImage.tsx
+++ components/post/ResponsiveImage.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 interface Props {
   src: string;
@@ -9,12 +10,25 @@ interface Props {
 }
 
 export function ResponsiveImage({ src, alt, width, height, description }: Props) {
-  return description ? (
-    <figure>
-      <Image className='rounded-md' priority src={src} alt={alt} width={+width} height={+height} />
-      <figcaption className='text-center'>{description}</figcaption>
-    </figure>
-  ) : (
-    <Image className='rounded-md' priority src={src} alt={alt} width={+width} height={+height} />
+  return (
+    <motion.div
+      initial={{
+        filter: 'blur(10px)',
+      }}
+      whileInView={{
+        filter: 'blur(0px)',
+      }}
+      transition={{ duration: 0.1 }}
+      viewport={{ once: true, amount: 0.5 }}
+    >
+      {description ? (
+        <figure>
+          <Image className='rounded-md' priority src={src} alt={alt} width={+width} height={+height} />
+          <figcaption className='text-center'>{description}</figcaption>
+        </figure>
+      ) : (
+        <Image className='rounded-md' priority src={src} alt={alt} width={+width} height={+height} />
+      )}
+    </motion.div>
   );
 }
