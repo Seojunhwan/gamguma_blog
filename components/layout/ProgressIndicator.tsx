d077a5a7a1833290c9319ae65983cdf0853e52af
Seojunhwan <seojunhwan@kakao.com>
2023-02-13 00:30:12 +0900

    design: remove scrollIndicator spring animation

--- components/layout/ProgressIndicator.tsx
+++ components/layout/ProgressIndicator.tsx
@@ -1,8 +1,22 @@
-import { motion, useScroll, useSpring } from 'framer-motion';
+import { motion, useScroll } from 'framer-motion';
 
 export const ProgressIndicator = () => {
   const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
 
-  return <motion.div className='absolute bottom-0 h-1 w-full origin-left bg-amber-300' style={{ scaleX }} />;
+  return (
+    <>
+      <motion.div
+        className='absolute bottom-0 h-1 w-full origin-left bg-amber-300'
+        style={{ scaleX: scrollYProgress }}
+      ></motion.div>
+      {/* TODO:둥둥떠다니기 */}
+      {/* <Image
+        className='absolute right-0 z-50 aspect-square h-4 w-4 '
+        src='/images/gumaguma.png'
+        alt='guma-indicator'
+        width={519.83}
+        height={498.61}
+      /> */}
+    </>
+  );
 };
