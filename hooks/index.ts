cddb33a8df634a29d481a9447a7e67d8e6e62342
Seojunhwan <seojunhwan@kakao.com>
2023-08-23 18:28:50 +0900

    fix: remove unnecessary files

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,5 +1,3 @@
-export * from './useTheme';
-export * from './useScroll';
-export * from './useLocalStorage';
 export * from './usePagination';
-export * from './useConst';
+export * from './useScroll';
+export * from './useTheme';
--- hooks/useConst.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from 'react';
-
-export const useConst = <T extends any>(init: T) => {
-  const ref = useRef<T | null>(null);
-  if (ref.current === null) {
-    ref.current = init;
-  }
-  return ref.current as T;
-};
