7b7c96243f7a0a626283d63558cddea8f5dcf598
Seojunhwan <seojunhwan@kakao.com>
2023-09-03 22:15:52 +0900

    fix: applyStylesByDataAttribute wrong return value

--- utils/style.ts
+++ utils/style.ts
@@ -13,22 +13,18 @@ interface ApplyStylesByDataAttribute {
 
 /**
  * tailwindcss 를 사용할 때 특정 data attribute 에 스타일을 적용할 수 있도록 도와주는 유틸 함수
- *
- * @param attribute
- * @param options the styles to apply to the data attribute or an object of key value pairs
- * @returns the tailwindcss plugin string
  */
 export const applyStylesByDataAttribute: ApplyStylesByDataAttribute = (attribute, options = []) => {
   if (typeof options === 'string') {
-    return `[data-${attribute}]:${options}`;
+    return `data-[${attribute}]:${options}`;
   }
 
   if (Array.isArray(options)) {
-    return options.reduce((acc, style) => `${acc} [data-${attribute}]:${style}`, '');
+    return options.reduce((acc, style) => `${acc} data-[${attribute}]:${style}`, '');
   }
 
   return Object.entries(options).reduce(
-    (acc, [key, value]) => `${acc} [data-${attribute}=${key}]:${value}`,
+    (acc, [key, value]) => `${acc} data-[${attribute}=${key}]:${value}`,
     '',
   );
 };
