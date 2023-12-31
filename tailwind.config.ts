1a27b4a1950359a98db3fb93bbcc1fa2ff9ac2d6
Seojunhwan <seojunhwan@kakao.com>
2023-12-31 20:47:48 +0900

    feat: disable typo config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,36 @@
 import type { Config } from 'tailwindcss';
 
+// Modify your tailwind.config.js
+const disabledCss = {
+  'code::before': false,
+  'code::after': false,
+  'blockquote p:first-of-type::before': false,
+  'blockquote p:last-of-type::after': false,
+  pre: false,
+  code: false,
+  'pre code': false,
+};
+
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './utils/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     fontFamily: {
       code: ['var(--font-d2coding)', 'var(--font-fira-code)'],
       body: ['var(--font-pretendard)', 'sans-serif'],
+    },
+    extend: {
+      typography: {
+        DEFAULT: { css: disabledCss },
+        sm: { css: disabledCss },
+        lg: { css: disabledCss },
+        xl: { css: disabledCss },
+        '2xl': { css: disabledCss },
+      },
     },
   },
   plugins: [require('@tailwindcss/typography')],
