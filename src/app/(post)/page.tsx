34804a13d0dd8701fe0d833723b317249b184afd
Seojunhwan <seojunhwan@kakao.com>
2024-01-01 23:24:07 +0900

    fix: remove /

--- src/app/(post)/page.tsx
+++ src/app/(post)/page.tsx
@@ -1,14 +1,16 @@
-import { getAllPost } from '@//utils/mdxUtils';
+import { getAllPost } from '@/utils/mdxUtils';
 import { PostCard } from './components/PostCard';
 
 export default function Home() {
   const posts = getAllPost();
 
   return (
-    <div className='mt-5'>
+    <ul className='mt-5'>
       {posts.map((post) => (
-        <PostCard key={post.slug} {...post} />
+        <li key={post.slug}>
+          <PostCard {...post} />
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
