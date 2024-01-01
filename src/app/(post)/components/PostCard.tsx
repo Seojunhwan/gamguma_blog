09888a8a384bc4cd19af98c5275d69425682189e
Seojunhwan <seojunhwan@kakao.com>
2024-01-01 23:52:36 +0900

    design: post card layout

--- src/app/(post)/components/PostCard.tsx
+++ src/app/(post)/components/PostCard.tsx
@@ -13,23 +13,23 @@ export const PostCard = ({ slug, metadata }: PostCardProps) => {
       className='mb-8 flex flex-col rounded-lg transition-all hover:bg-neutral-100 dark:hover:bg-gray-200 dark:active:dark:hover:bg-gray-200'
     >
       <Link href={`/post/${slug}`} className='w-full p-3'>
-        <div className='flex'>
-          <div className='flex grow flex-col gap-1'>
+        <div className='flex flex-col gap-1'>
+          <div className='flex items-center justify-between'>
             <h2 className='break-keep text-base font-medium tracking-tight text-neutral-900 dark:text-gray-1200'>
               {metadata.title}
             </h2>
-            <p className='line-clamp-1 text-sm text-neutral-700 dark:text-gray-1000'>
-              {metadata.description}
-            </p>
-          </div>
-
-          <div className='flex flex-col items-end justify-between whitespace-nowrap'>
             <time
               dateTime={metadata.createAt}
-              className='text-xs font-light text-neutral-600 dark:text-gray-1100'
+              className='whitespace-nowrap text-xs font-light text-neutral-600 dark:text-gray-1100'
             >
               {getRelativeDate(metadata.createAt)}
             </time>
+          </div>
+
+          <div className='flex items-center justify-between'>
+            <p className='line-clamp-1 text-sm text-neutral-700 dark:text-gray-1000'>
+              {metadata.description}
+            </p>
             <Suspense fallback={<Views.Loader />}>
               <Views slug={slug} />
             </Suspense>
