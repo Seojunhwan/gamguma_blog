9e289f1e07063d2104f6e497d3bbbef759622fe6
Seojunhwan <seojunhwan@kakao.com>
2024-01-01 23:48:04 +0900

    fix: remove header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import { D2Coding, firaCode, pretendard } from '@//styles/font';
+import { D2Coding, firaCode, pretendard } from '@/styles/font';
 import '@/styles/globals.css';
 import { type PropsWithChildren } from 'react';
-import { Footer } from './layout/Footer';
-import { Header } from './layout/Header';
+import { Footer } from './components';
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang='ko'>
-      <body className={`${pretendard.variable} ${firaCode.variable} ${D2Coding.variable} font-body`}>
-        <Header />
-        <main className='mx-auto w-full max-w-screen-lg px-2 pb-10 pt-16 sm:px-5 sm:pb-14 sm:pt-20'>
-          {children}
-        </main>
-        <Footer />
+      <body
+        className={`${pretendard.variable} ${firaCode.variable} ${D2Coding.variable} bg-white font-body antialiased dark:bg-[#111110]`}
+      >
+        <div className='mx-auto max-w-2xl '>
+          <main className='mx-auto w-full max-w-screen-lg pb-10 pt-16 sm:pb-14 sm:pt-20'>{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
