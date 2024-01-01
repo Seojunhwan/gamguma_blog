55963cef8b7fd84f3769fdab965e9afe3dd2b468
Seojunhwan <seojunhwan@kakao.com>
2024-01-01 23:24:55 +0900

    design: add color token

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,22 @@ const config: Config = {
       body: ['var(--font-pretendard)', 'sans-serif'],
     },
     extend: {
+      colors: {
+        gray: {
+          100: '#111111',
+          200: '#1a1a1a',
+          300: '#222222',
+          400: '#2b2b2b',
+          500: '#313131',
+          600: '#3b3b3b',
+          700: '#494949',
+          800: '#626262',
+          900: '#6f6f6f',
+          1000: '#7f7f7f',
+          1100: '#b5b3ad',
+          1200: '#eeeeee',
+        },
+      },
       typography: {
         DEFAULT: { css: disabledCss },
         sm: { css: disabledCss },
