a09864ec6564f7542fecd23d90acd168e0d4dcbe
seojunhwan <seojunhwan@kakao.com>
2024-04-21 20:44:06 +0900

    feat: update next canary & enable partial rendering

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    ppr: true,
+  },
+  transpilePackages: ['next-mdx-remote'],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
