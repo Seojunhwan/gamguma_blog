eb93b009a9ee5d82321bb11275bde224ba4787b2
seojunhwan <seojunhwan@kakao.com>
2024-04-21 17:48:44 +0900

    feat: photo, craft

--- src/app/(post)/layout.tsx
+++ src/app/(post)/layout.tsx
@@ -4,10 +4,23 @@ import { type PropsWithChildren } from 'react';
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <>
-      <header className='mb-8'>
+      <header className='mb-8 flex items-center gap-2'>
         <h1 className='p-3 text-lg font-semibold text-neutral-900 dark:text-gray-1200'>
-          <Link href='/'>Gamguma</Link>
+          <Link href='/'>gamguma</Link>
         </h1>
+        <nav>
+          <ul className='flex gap-2'>
+            <li>
+              <Link href='/about'>about</Link>
+            </li>
+            <li>
+              <Link href='/photo'>photo</Link>
+            </li>
+            <li>
+              <Link href='/craft'>craft</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       {children}
     </>
---
+++ src/app/photo/page.tsx
@@ -0,0 +1,3 @@
+export default function PhotoPage() {
+  return <div>뚝딱뚝딱</div>;
+}
