b5a857161e4c5a379a4edcf4bc9461791e8c8176
seojunhwan <seojunhwan@kakao.com>
2024-05-12 21:55:49 +0900

    style: media query dark mode color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ import { BASE_URL } from '@/constants/url';
 export const viewport: Viewport = {
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: '(prefers-color-scheme: dark)', color: '#111110' },
   ],
   userScalable: false,
 };
