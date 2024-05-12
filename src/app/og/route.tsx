afc82174a0851e1f204dce31b78ece2a4642e440
seojunhwan <seojunhwan@kakao.com>
2024-05-12 18:28:36 +0900

    fix: migrate edge to node

--- src/app/og/route.tsx
+++ src/app/og/route.tsx
@@ -1,15 +1,13 @@
 import { BASE_URL } from '@/constants/url';
 import { ImageResponse } from 'next/og';
 import { type NextRequest } from 'next/server';
+import fs from 'fs/promises';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
 export async function GET(req: NextRequest) {
   const title = req.nextUrl.searchParams.get('title');
-  const font = fetch(new URL('../../../public/fonts/gangwon_bold.woff', import.meta.url)).then((res) =>
-    res.arrayBuffer(),
-  );
-  const fontData = await font;
+  const font = await fs.readFile(process.cwd() + '/public/fonts/gangwon_bold.woff');
 
   return new ImageResponse(
     (
@@ -42,7 +40,7 @@ export async function GET(req: NextRequest) {
       fonts: [
         {
           name: 'Gangwon',
-          data: fontData,
+          data: font,
           style: 'normal',
         },
       ],
