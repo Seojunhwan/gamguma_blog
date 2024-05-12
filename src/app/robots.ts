281d813342d31f42b10fe9c452d001e1ae36e68d
seojunhwan <seojunhwan@kakao.com>
2024-05-12 16:17:24 +0900

    feat: icon

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,12 @@
 import { MetadataRoute } from 'next';
-import { BASE_URL } from './constants/url';
+import { BASE_URL } from '../constants/url';
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
+      disallow: '/tags',
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
