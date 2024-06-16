298c99a7b26bb6cdfe4db4e822b9bd48181cf050
seojunhwan <seojunhwan@kakao.com>
2024-06-16 15:13:24 +0900

    refactor: button

--- src/components/common/button/button.tsx
+++ src/components/common/button/button.tsx
@@ -1,44 +1,34 @@
 import { cn } from '@/utils/cn';
 import { type ButtonHTMLAttributes } from 'react';
 
-// type ButtonVariant = 'solid' | 'outline' | 'ghost';
-// type ButtonColor = 'primary' | 'secondary' | 'danger';
+type ButtonVariant = 'solid' | 'outline' | 'ghost' | 'link';
 type ButtonSize = 'sm' | 'md' | 'lg';
 type ButtonRadius = 'sm' | 'md' | 'lg';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  // variant?: ButtonVariant;
-  // color?: ButtonColor;
+  variant?: ButtonVariant;
   size?: ButtonSize;
   radius?: ButtonRadius;
 }
 
-/**
- * TODO: Add variantColorClasses
- * variant, color가 정의되지 않아 우선 외부에서 주입받아 사용하도록 하며 하기 코드는 주석처리
- */
-// const variantColorClasses: Record<`${Variant}-${Color}`, string> = {
-//   'solid-primary': 'bg-primary text-gray-700',
-//   'solid-secondary': 'bg-secondary text-gray-700',
-//   'solid-danger': 'bg-danger text-gray-700',
-//   'outline-primary': 'border border-primary text-primary',
-//   'outline-secondary': 'border border-secondary text-secondary',
-//   'outline-danger': 'border border-danger text-danger',
-//   'ghost-primary': 'text-primary',
-//   'ghost-secondary': 'text-secondary',
-//   'ghost-danger': 'text-danger',
-// };
+const variantClasses: Record<ButtonVariant, string> = {
+  solid: 'bg-neutral-950 text-gray-1200 dark:bg-gray-1200 dark:text-gray-950',
+  ghost: 'text-neutral-950 hover:bg-neutral-100 dark:text-gray-1200 dark:hover:bg-gray-300',
+  outline:
+    'border border-neutral-200 text-neutral-950 hover:bg-neutral-100 dark:border-neutral-800 dark:text-gray-1200 dark:hover:bg-gray-300',
+  link: 'p-0 underline underline-offset-4 dark:text-gray-1200 dark:hover:text-gray-1100',
+};
 
 const sizeClasses: Record<ButtonSize, string> = {
   sm: 'px-2 py-1 text-sm',
   md: 'px-4 py-2 text-base',
-  lg: 'px-6 py-3 text-lg',
+  lg: 'px-4 py-2 text-lg',
 };
 
 const radiusClasses: Record<ButtonRadius, string> = {
-  sm: 'rounded-sm',
-  md: 'rounded-md',
-  lg: 'rounded-lg',
+  sm: 'rounded-[4px]',
+  md: 'rounded-[6px]',
+  lg: 'rounded-[10px]',
 };
 
 const defaultClasses = [
@@ -46,24 +36,19 @@ const defaultClasses = [
   'transition duration-200 ease-in-out',
   'focus:outline-none',
   'disabled:opacity-50 disabled:cursor-not-allowed',
+  'font-normal',
 ];
 
 export const Button = ({
   children,
-  // variant = 'solid',
-  // color = 'primary',
+  variant = 'solid',
+  color = 'primary',
   size = 'sm',
   radius = 'sm',
   className,
   ...restProps
 }: ButtonProps) => {
-  const classes = [
-    //variant, color가 정의되지 않아 우선 외부에서 주입받아 사용하도록 하며 하기 코드는 주석처리
-    // variantColorClasses[`${variant}-${color}`],
-    sizeClasses[size],
-    radiusClasses[radius],
-    className,
-  ];
+  const classes = [sizeClasses[size], variantClasses[variant], radiusClasses[radius], className];
   return (
     <button className={cn(defaultClasses, classes)} {...restProps}>
       {children}
