dba7fb7130145cd8fa73a7837754ebe1070d521c
seojunhwan <seojunhwan@kakao.com>
2024-06-16 15:16:39 +0900

    fix: image remote pattern

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_CDN_URL,
+        hostname: process.env.NEXT_PUBLIC_CDN_URL.split('://')[1],
       },
     ],
   },
