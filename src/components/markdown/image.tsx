9e26a686c7f9d047dcdd1337cfb3982e474082b0
seojunhwan <seojunhwan@kakao.com>
2024-12-22 23:26:55 +0900

    docs: 회고-1

--- src/components/markdown/image.tsx
+++ src/components/markdown/image.tsx
@@ -1,5 +1,18 @@
 import Image from 'next/image';
 
-export const CustomImage = (props: any) => {
-  return <Image alt={props.alt} className='rounded-md' {...props} />;
+interface CustomImageProps extends React.ComponentProps<typeof Image> {
+  description?: string;
+}
+
+export const CustomImage = ({ description, alt, ...props }: CustomImageProps) => {
+  return (
+    <figure className='my-4'>
+      <Image alt={alt} className='w-full rounded-md' {...props} />
+      {description && (
+        <figcaption className='mt-2 text-center text-sm text-gray-500 dark:text-neutral-400'>
+          {description}
+        </figcaption>
+      )}
+    </figure>
+  );
 };
