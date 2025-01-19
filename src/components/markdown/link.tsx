5b3c6b9e1295aa9b3a82d8bfd95ffdb9b03dab2f
Seojunhwan <seojunhwan@kakao.com>
2025-01-19 22:57:49 +0900

    chore: update next-view-transitions to version 0.3.4 in package.json and pnpm-lock.yaml, enhancing compatibility with Next.js 14.3.0-canary.14; refactor CustomLink component to handle hash links in addition to relative paths

--- src/components/markdown/link.tsx
+++ src/components/markdown/link.tsx
@@ -4,12 +4,8 @@ import type { AnchorHTMLAttributes } from 'react';
 interface CustomLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {}
 
 export const CustomLink = ({ href, ...restProps }: CustomLinkProps) => {
-  if (href?.startsWith('/')) {
+  if (href?.startsWith('/') || href?.startsWith('#')) {
     return <Link href={href} {...restProps} />;
-  }
-
-  if (href?.startsWith('#')) {
-    return <a href={href} {...restProps} />;
   }
 
   return <a href={href} target='_blank' rel='noopener noreferrer' {...restProps} />;
